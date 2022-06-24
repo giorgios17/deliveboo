a1363ff1c0f3c9d7715a56358abaa03f552ee5aa
giorgios17 <giosur@live.it>
2022-06-24 17:35:47 +0200

    style dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                @endif
+        <div class="col-md-8">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
+            <div class="d-flex justify-content-between">
                 <div>
-                    Benvenuto, {{ Auth::user()->business_name }}
-                    <img class="w-50" src="{{ asset('storage/' . Auth::user()->image) }}" alt="">
+                    <h2>
+                        Benvenuto, {{ Auth::user()->business_name }}
+                    </h2>
                     <p>{{ Auth::user()->address }}</p>
                     <p>{{ Auth::user()->phone }}</p>
                     <p>{{ Auth::user()->vat_number }}</p>
@@ -20,6 +21,9 @@
                     <p>{{ Auth::user()->closing_day }}</p>
                     <p>{{ Auth::user()->free_shipping }}</p>
                     <p>{{ Auth::user()->shipping_price }}</p>
+                </div>
+                <div>
+                    <img class="w-50" src="{{ asset('storage/' . Auth::user()->image) }}" alt="">
                 </div>
             </div>
         </div>
