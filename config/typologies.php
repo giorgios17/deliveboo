e4a687550e035d0dea6c05e68118d6fa8eea9148
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-24 19:51:02 +0200

    completato array tipologie

--- config/typologies.php
+++ config/typologies.php
@@ -1,79 +1,73 @@
 <?php
-
 return [
     [
         "name" => "Pizzeria",
         "description" => "Farina, acqua e lievito: la regina della tavola!",
-        "image" => '/images/pizza.png'
+        "image" => '/images/typologies/pizzeria.png'
     ],
-
     [
         "name" => "Italiano",
         "description" => "Pasta, carne, pesce, a te la scelta",
-        "image" => '/images/italiano.png',
+        "image" => '/images/typologies/italiano.png',
     ],
     [
         "name" => "Cinese",
         "description" => "Si fa amare facilmente: varia, speziata, condita, saporita e con molte salse",
-        "image" => '',
+        "image" => '/images/typologies/cinese.png'
     ],
     [
         "name" => "Giapponese",
         "description" => "Riso per sushi, l'aceto di riso e vai in Oriente",
-        "image" => '/images/sushi-1.png',
+        "image" => '/images/typologies/giapponese.png',
     ],
     [
         "name" => "Messicano",
         "description" => "Gusto intenso e piccante",
-        "image" => '',
+        "image" => '/images/typologies/messicano.png'
     ],
     [
         "name" => "Indiano",
         "description" => "Sapori intensi, speziati e ricchi di aromi",
-        "image" => '',
+        "image" => '/images/typologies/indiano.png'
     ],
     [
         "name" => "Pesce",
         "description" => "La cucina a base di pesce",
-        "image" => "",
+        "image" => '/images/typologies/pesce.png'
     ],
     [
         "name" => "Carne",
         "description" => "La cucina a base di carne è basata su ingredienti animali",
-        "image" => "",
+        "image" => '/images/typologies/carne.png'
     ],
     [
         "name" => "Vegetariano",
         "description" => "NO carne! NO pesce!",
-        "image" => '/images/organic.png',
+        "image" => '/images/typologies/vegetariano.png',
     ],
     [
         "name" => "Senza Glutine",
         "description" => "Totale assenza di glutine",
-        "image" => "",
+        "image" => '/images/typologies/senza_glutine.png'
     ],
     [
         "name" => "Americano",
-        "description" => "",
-        "image" => '/images/burgers-1.png',
+        "description" => "Hai voglia di americano? Scegli tra hamburger, donunts, pancakes",
+        "image" => '/images/typologies/americano.png',
     ],
     [
         "name" => "Dessert",
         "description" => "",
-        "image" => '/images/dessert.png',
+        "image" => '/images/typologies/dessert.png',
     ],
     [
         "name" => "Gelato",
-        "description" => "",
-        "image" => '/images/ice-cream.png',
+        "description" => "Come terminare una cena? Con un ottimo gelato!",
+        "image" => '/images/typologies/gelato.png',
     ],
     [
         "name" => "Poke",
-        "description" => "",
-        "image" => '/images/poke.png',
+        "description" => "Pesce crudo, verdura, frutta e salsa: il vero pokè!",
+        "image" => '/images/typologies/poke.png',
     ],
-
-]
-
-
-?>
+];
