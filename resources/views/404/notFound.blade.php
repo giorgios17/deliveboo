fb8700961c4ddf24034e2944d2b0bc98a92c1cbc
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-25 11:21:45 +0200

    creata view not Found

---
+++ resources/views/404/notFound.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('404 Not Found') }}</div>
+
+                <div class="card-body">
+                    <p>The page you are looking for does not exist.</p>
+                </div>
+                <a class="btn btn-primary" href="/">Torna alla Home</a>
+            </div>
+        </div>
+    </div>
+@endsection
