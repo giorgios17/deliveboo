83c247ad4319349efde5aa31ec36d9a1e308dc67
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-26 15:34:26 +0200

    aggiunto seeder paitti

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TypologyTableSeeder::class,
             UsersTableSeeder::class,
+            PlatesTableSeeder::class
         ]);
     }
 }
