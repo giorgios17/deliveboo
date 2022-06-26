de70056546d700cbab5e4f98b7bc05cfbf427e3c
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-26 15:34:12 +0200

    creata prima bozza piatti

---
+++ config/plates.php
@@ -0,0 +1,76 @@
+<?php
+
+return [
+    [
+        "name"  => "Margherita",
+        "description" => "La pizza Margherita è la regina delle pizze, condita con pomodoro, mozzarella, basilico fresco, sale e olio",
+        "price" => "5.00",
+        "visible" => "1",
+        "image" => "images/plates/pizza-margherita.jpg",
+        "user_id" => "1"
+    ],
+    [
+        "name"  => "Diavola",
+        "description" => "Variante della Margherita, divenuta anch’essa negli ultimi anni un classico, condita con pomodoro, mozzarella, salame piccante, peperoncino e olive nere",
+        "price" => "6.50",
+        "visible" => "1",
+        "image" => "images/plates/pizza-alla-diavola.jpg",
+        "user_id" => "1"
+    ],
+    [
+        "name"  => "Crocchè",
+        "description" => "La pizza crocchè è morbida e soffice, farcita con mozzarella, prosciutto cotto e crocchette di patate",
+        "price" => "7.00",
+        "visible" => "1",
+        "image" => "images/plates/pizza-crocchè.jpg",
+        "user_id" => "1"
+    ],
+    [
+        "name"  => "Margherita",
+        "description" => "La pizza Margherita è la regina delle pizze, condita con pomodoro, mozzarella, basilico fresco, sale e olio",
+        "price" => "5.00",
+        "visible" => "1",
+        "image" => "images/plates/pizza-margherita.jpg",
+        "user_id" => "2"
+    ],
+    [
+        "name"  => "Capricciosa",
+        "description" => "La pizza capricciosa è una pizza tipica della cucina italiana caratterizzata da un condimento di pomodoro, mozzarella, prosciutto cotto (o spesso anche crudo), funghi (di solito champignon), olive verdi e nere, e carciofini e spesso uova.",
+        "price" => "7.00",
+        "visible" => "1",
+        "image" => "images/plates/pizza-capricciosa.jpg",
+        "user_id" => "2"
+    ],
+    [
+        "name"  => "Crocchè",
+        "description" => "La pizza crocchè è morbida e soffice, farcita con mozzarella, prosciutto cotto e crocchette di patate",
+        "price" => "7.00",
+        "visible" => "1",
+        "image" => "images/plates/pizza-crocchè.jpg",
+        "user_id" => "2"
+    ],
+    [
+        "name"  => "Nigiri di salmone",
+        "description" => "Pallina ovale di riso, modellata a mano e guarnita da una fettina sottile di salmone",
+        "price" => "5.00",
+        "visible" => "1",
+        "image" => "images/plates/nigiri-salmone.jpg",
+        "user_id" => "3"
+    ],
+    [
+        "name"  => "Nigiri di tonno",
+        "description" => "Pallina ovale di riso, modellata a mano e guarnita da una fettina sottile di tonno",
+        "price" => "5.00",
+        "visible" => "1",
+        "image" => "images/plates/nigiri-tonno.jpg",
+        "user_id" => "3"
+    ],
+    [
+        "name"  => "Uramaki",
+        "description" => "Uramaki in giapponese significa rotolo “dentro e fuori”.",
+        "price" => "7.00",
+        "visible" => "1",
+        "image" => "images/plates/uramaki.jpg",
+        "user_id" => "3"
+    ],
+];
