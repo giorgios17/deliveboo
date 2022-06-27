284f88034f67be96c9d959bfabae167ef9f83457
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-27 22:29:16 +0200

    creata view alternativa per la registrazione, da collegare

---
+++ resources/views/admin/indexRegister.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="card-title">Benvenuto {{ Auth::user()->business_name }}</h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">
+                            {{-- box alert registrazione effettuata con successo --}}
+                        <div class="alert alert-success" role="alert">
+                            Congratulazioni {{ Auth::user()->business_name }}, la registrazione ha avuto successo.
+                        </div>
+                        </p>
+                        <a href="admin/user" class="btn btn-success">Vai alla Dashboard</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
