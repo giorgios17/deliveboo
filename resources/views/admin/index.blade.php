fb96120b16a2bece7c086ff2c3dec00262514a74
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-27 22:29:01 +0200

    rilavorato layout

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,17 +1,22 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
+    <div class="container my-5">
         <div class="row justify-content-center">
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title">Benvenuto</h5>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">
-                        {{ Auth::user()->business_name }}
-                        hai effettuato il login!
-                    </p>
-                    <a href="admin/user" class="btn btn-primary">Vai alla Dashboard</a>
+            <div class="col-md-8">
+
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="card-title">Benvenuto {{ Auth::user()->business_name }}</h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">
+                            {{-- box alert registrazione effettuata con successo --}}
+                        <div class="alert alert-success" role="alert">
+                            Congratulazioni {{ Auth::user()->business_name }}, il login ha avuto successo.
+                        </div>
+                        </p>
+                        <a href="admin/user" class="btn btn-success">Vai alla Dashboard</a>
+                    </div>
                 </div>
             </div>
         </div>
