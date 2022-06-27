188e4541563c9f763880fe2dd710c8044501f5fe
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-27 17:25:47 +0200

    rimodellato layout login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,25 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container my-5">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
+                    <div class="card-header">{{ __('Accedi') }}</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('login') }}">
+                            {{-- token --}}
                             @csrf
 
+                            {{-- box alert campi obbligatori --}}
+                            <div class="alert alert-warning" role="alert">
+                                I campi contrassegnati dall'asterisco (*) sono obbligatori.
+                            </div>
+
+                            {{-- input indirizzo email --}}
                             <div class="form-group row">
                                 <label for="email"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo E-Mail') }}
+                                    <span class="text-warning">*</span></label>
 
                                 <div class="col-md-6">
                                     <input id="email" type="email"
                                         class="form-control @error('email') is-invalid @enderror" name="email"
-                                        value="{{ old('email') }}" required autocomplete="email" autofocus>
-
+                                        value="{{ old('email') }}" required autocomplete="email"
+                                        placeholder="Inserisci la mail" autofocus>
                                     @error('email')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -28,15 +36,15 @@
                                 </div>
                             </div>
 
+                            {{-- input password --}}
                             <div class="form-group row">
-                                <label for="password"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}
+                                    <span class="text-warning">*</span></label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="current-password">
-
+                                        required autocomplete="password" placeholder="Inserisci la password">
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -45,6 +53,7 @@
                                 </div>
                             </div>
 
+                            {{-- checkbox ricordami --}}
                             <div class="form-group row">
                                 <div class="col-md-6 offset-md-4">
                                     <div class="form-check">
@@ -52,21 +61,22 @@
                                             {{ old('remember') ? 'checked' : '' }}>
 
                                         <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
+                                            {{ __('Ricordami') }}
                                         </label>
                                     </div>
                                 </div>
                             </div>
 
+                            {{-- bottone accedi --}}
                             <div class="form-group row mb-0">
                                 <div class="col-md-8 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('Login') }}
+                                        {{ __('Accedi') }}
                                     </button>
 
                                     @if (Route::has('password.request'))
                                         <a class="btn btn-link" href="{{ route('password.request') }}">
-                                            {{ __('Forgot Your Password?') }}
+                                            {{ __('Password dimenticata?') }}
                                         </a>
                                     @endif
                                 </div>
