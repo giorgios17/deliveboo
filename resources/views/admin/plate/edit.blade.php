43d56c967ca9e12a98e63a26a4ed9abe6dd4056d
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-27 22:10:51 +0200

    cambiato colore button e aggiunto margin-y

--- resources/views/admin/plate/edit.blade.php
+++ resources/views/admin/plate/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container my-5">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
@@ -109,7 +109,7 @@
                             {{-- bottone di submit dati --}}
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                                    <button type="submit" class="btn btn-warning">
                                         {{ __('Modifica piatto') }}
                                     </button>
                                 </div>
