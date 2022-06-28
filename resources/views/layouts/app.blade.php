d29cf03a62fd06d8f64d8da77386a2953f0fcb15
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-28 21:45:53 +0200

    resa navbar fissa e aggiunta parte sinistra del layout di default

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,69 +32,106 @@
 
 <body>
     <div id="app" class="layout_base">
-        <nav class="navbar navbar-expand-md shadow-sm">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <div class="wrapper_logo">
-                        <img class="w-100" src="{{ asset('images/deliveboo-logo-green.png') }}" alt="">
-                    </div>
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
-                    {{-- <span class="navbar-toggler-icon"> <i class="fa-solid fa-bars"></i></span> --}}
-                    <div class="navbar-toggler-icon d-flex align-items-center justify-content-center"> <i
-                            class="fa-solid fa-bars"></i></div>
-                </button>
+        <div class="position-fixed w-100">
+            <nav class="navbar navbar-expand-md shadow-sm">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        <div class="wrapper_logo">
+                            <img class="w-100" src="{{ asset('images/deliveboo-logo-green.png') }}" alt="">
+                        </div>
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        {{-- <span class="navbar-toggler-icon"> <i class="fa-solid fa-bars"></i></span> --}}
+                        <div class="navbar-toggler-icon d-flex align-items-center justify-content-center"> <i
+                                class="fa-solid fa-bars"></i></div>
+                    </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav mr-auto">
 
-                    </ul>
+                        </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-
-                                <a class="nav-link font-weight-bold" href="{{ route('login') }}"><i
-                                        class="fa-solid fa-arrow-right-to-bracket"></i> {{ __('Accedi') }} </a>
-                            </li>
-                            @if (Route::has('register'))
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            @guest
                                 <li class="nav-item">
-                                    <a class="nav-link font-weight-bold" href="{{ route('register') }}">
-                                        <i class="fa-solid fa-pen-to-square"></i> {{ __('Registrati') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                    <a class="nav-link font-weight-bold" href="{{ route('login') }}"><i
+                                            class="fa-solid fa-arrow-right-to-bracket"></i> {{ __('Accedi') }} </a>
+                                </li>
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link font-weight-bold" href="{{ route('register') }}">
+                                            <i class="fa-solid fa-pen-to-square"></i> {{ __('Registrati') }}</a>
+                                    </li>
+                                @endif
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                         document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                            class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
 
         <main>
-            @yield('content')
+            <div id="user_index" class="container-fluid">
+                <div class="row">
+                    <div class="col-md-4 col-xl-3 restaurant_bg">
+                        <div class="d-flex flex-column align-items-center">
+                            <div class="restaurant_img mt-5 mb-4">
+                                <img src="{{ asset('storage/' . Auth::user()->image) }}" alt="">
+                            </div>
+                            <h2 class="mb-5 text-white">
+                                {{ Auth::user()->business_name }}
+                            </h2>
+                            <a class="btn btn_dashboard mb-3" href="{{ route('admin.user.index') }}"><i
+                                    class="fa-solid fa-gear"></i> Dashboard</a>
+                            <a class="btn btn_dashboard mb-3" href="{{ route('admin.plate.create') }}"><i
+                                    class="fa-solid fa-plus"></i> Aggiungi un
+                                piatto</a>
+                            <a class="btn btn_dashboard mb-3" href="{{ route('admin.plate.index') }}"><i
+                                    class="fa-solid fa-magnifying-glass"></i> Visualizza i
+                                tuoi piatti</a>
+                            <a class="btn btn-danger mb-5" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();"><i
+                                    class="fa-solid fa-arrow-right-from-bracket"></i>
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-md-8 col-xl-9 pt-3 right_column">
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
         </main>
     </div>
 </body>
