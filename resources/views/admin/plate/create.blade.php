8b02b35fddb6d2689d22582ff9d40e22654a22de
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-28 21:44:57 +0200

    rimosso container div

--- resources/views/admin/plate/create.blade.php
+++ resources/views/admin/plate/create.blade.php
@@ -1,112 +1,108 @@
 @extends('layouts.app')
 
 @section('content')
-    <div id="plate_create" class="container my-5">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Nuovo piatto') }}</div>
+    <div id="plate_create" class="row justify-content-center py-5">
+        <div class="col-md-10 col-lg-9 col-xl-8">
+            <div class="card">
+                <div class="card-header">{{ __('Nuovo piatto') }}</div>
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('admin.plate.store') }}" enctype="multipart/form-data">
-                            {{-- token --}}
-                            @csrf
+                <div class="card-body">
+                    <form method="POST" action="{{ route('admin.plate.store') }}" enctype="multipart/form-data">
+                        {{-- token --}}
+                        @csrf
 
-                            {{-- box alert campi obbligatori --}}
-                            <div class="alert alert-warning" role="alert">
-                                I campi contrassegnati dall'asterisco (*) sono obbligatori.
+                        {{-- box alert campi obbligatori --}}
+                        <div class="alert alert-warning" role="alert">
+                            I campi contrassegnati dall'asterisco (*) sono obbligatori.
+                        </div>
+
+                        {{-- input nome piatto --}}
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome piatto') }}
+                                <span class="text-warning">*</span></label>
+                            <div class="col-md-6">
+                                <input id="name" type="text"
+                                    class="form-control @error('name') is-invalid @enderror" name="name"
+                                    value="{{ old('name') }}" required autocomplete="name"
+                                    placeholder="Inserisci il nome del piatto" autofocus>
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
+                        </div>
 
-                            {{-- input nome piatto --}}
-                            <div class="form-group row">
-                                <label for="name"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Nome piatto') }}
-                                    <span class="text-warning">*</span></label>
-                                <div class="col-md-6">
-                                    <input id="name" type="text"
-                                        class="form-control @error('name') is-invalid @enderror" name="name"
-                                        value="{{ old('name') }}" required autocomplete="name"
-                                        placeholder="Inserisci il nome del piatto" autofocus>
-                                    @error('name')
+                        {{-- input descrizione piatto --}}
+                        <div class="form-group row">
+                            <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('Descrizione') }}
+                                <span class="text-warning">*</span></label>
+                            <div class="col-md-6">
+                                <textarea class="form-control @error('description') is-invalid @enderror" id="exampleFormControlTextarea1"
+                                    rows="3" name="description" required autocomplete="description" minlength="10"
+                                    placeholder="Inserisci la descrizione del piatto">{{ old('description') }}</textarea>
+                                @error('description')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        {{-- input prezzo piatto --}}
+                        <div class="form-group row mt-3">
+                            <label for="price" class="col-md-4 col-form-label text-md-right">{{ __('Prezzo') }}
+                                <span class="text-warning">*</span></label>
+                            <div class="col-md-6">
+                                <input id="price" type="number" min="0.1" step=".01"
+                                    class="form-control @error('price') is-invalid @enderror" name="price"
+                                    value="{{ old('price') }}" required autocomplete="price" placeholder="10.00">
+                                @error('price')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        {{-- select visibilità piatto --}}
+                        <div class="form-group row">
+                            <label for="visible" class="col-md-4 col-form-label text-md-right">{{ __('Visibile') }}
+                                <span class="text-warning">*</span></label>
+                            <div class="col-md-6">
+                                <select class="form-control @error('closing_day') is-invalid @enderror" name="visible"
+                                    required autocomplete="visible">
+                                    <option value="1">Si</option>
+                                    <option value="0">No</option>
+                                    @error('visible')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
-                                </div>
+                                </select>
                             </div>
+                        </div>
 
-                            {{-- input descrizione piatto --}}
-                            <div class="form-group row">
-                                <label for="description"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Descrizione') }}
-                                    <span class="text-warning">*</span></label>
-                                <div class="col-md-6">
-                                    <textarea class="form-control @error('description') is-invalid @enderror" id="exampleFormControlTextarea1"
-                                        rows="3" name="description" required autocomplete="description" minlength="10"
-                                        placeholder="Inserisci la descrizione del piatto">{{ old('description') }}</textarea>
-                                    @error('description')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
+                        {{-- input immagine piatto --}}
+                        <div class="form-group row">
+                            <label for="image"
+                                class="col-md-4 col-form-label text-md-right">{{ __('Immagine ristorante') }}
+                                <span class="text-warning">*</span>
+                            </label>
+                            <div class="col-md-6">
+                                <input type="file" class="form-control-file" name="image" required>
                             </div>
+                        </div>
 
-                            {{-- input prezzo piatto --}}
-                            <div class="form-group row mt-3">
-                                <label for="price" class="col-md-4 col-form-label text-md-right">{{ __('Prezzo') }}
-                                    <span class="text-warning">*</span></label>
-                                <div class="col-md-6">
-                                    <input id="price" type="number" min="0.1" step=".01"
-                                        class="form-control @error('price') is-invalid @enderror" name="price"
-                                        value="{{ old('price') }}" required autocomplete="price" placeholder="10.00">
-                                    @error('price')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
+                        {{-- bottone di submit dati --}}
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-warning">
+                                    {{ __('Crea piatto') }}
+                                </button>
                             </div>
-
-                            {{-- select visibilità piatto --}}
-                            <div class="form-group row">
-                                <label for="visible" class="col-md-4 col-form-label text-md-right">{{ __('Visibile') }}
-                                    <span class="text-warning">*</span></label>
-                                <div class="col-md-6">
-                                    <select class="form-control @error('closing_day') is-invalid @enderror" name="visible"
-                                        required autocomplete="visible">
-                                        <option value="1">Si</option>
-                                        <option value="0">No</option>
-                                        @error('visible')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </select>
-                                </div>
-                            </div>
-
-                            {{-- input immagine piatto --}}
-                            <div class="form-group row">
-                                <label for="image"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Immagine ristorante') }}
-                                    <span class="text-warning">*</span>
-                                </label>
-                                <div class="col-md-6">
-                                    <input type="file" class="form-control-file" name="image" required>
-                                </div>
-                            </div>
-
-                            {{-- bottone di submit dati --}}
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-warning">
-                                        {{ __('Crea piatto') }}
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
--- resources/views/admin/plate/edit.blade.php
+++ resources/views/admin/plate/edit.blade.php
@@ -1,121 +1,117 @@
 @extends('layouts.app')
 
 @section('content')
-    <div id="plate_edit" class="container my-5">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Modifica piatto') }}</div>
+    <div id="plate_edit" class="row justify-content-center py-5">
+        <div class="col-md-10 col-lg-9 col-xl-8">
+            <div class="card">
+                <div class="card-header">{{ __('Modifica piatto') }}</div>
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('admin.plate.update', $plate->id) }}"
-                            enctype="multipart/form-data">
-                            {{-- token --}}
-                            @csrf
-                            {{-- cambio il metodo di invio dei dati in PUT --}}
-                            @method('PUT')
+                <div class="card-body">
+                    <form method="POST" action="{{ route('admin.plate.update', $plate->id) }}"
+                        enctype="multipart/form-data">
+                        {{-- token --}}
+                        @csrf
+                        {{-- cambio il metodo di invio dei dati in PUT --}}
+                        @method('PUT')
 
-                            {{-- box alert campi obbligatori --}}
-                            <div class="alert alert-warning" role="alert">
-                                I campi contrassegnati dall'asterisco (*) sono obbligatori.
+                        {{-- box alert campi obbligatori --}}
+                        <div class="alert alert-warning" role="alert">
+                            I campi contrassegnati dall'asterisco (*) sono obbligatori.
+                        </div>
+
+                        {{-- input nome piatto --}}
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome piatto') }}
+                                <span class="text-warning">*</span></label>
+                            <div class="col-md-6">
+                                <input id="name" type="text"
+                                    class="form-control @error('name') is-invalid @enderror" name="name"
+                                    value="{{ old('name', $plate->name) }}" required autocomplete="name"
+                                    placeholder="Inserisci il nome del piatto" autofocus>
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
+                        </div>
 
-                            {{-- input nome piatto --}}
-                            <div class="form-group row">
-                                <label for="name"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Nome piatto') }}
-                                    <span class="text-warning">*</span></label>
-                                <div class="col-md-6">
-                                    <input id="name" type="text"
-                                        class="form-control @error('name') is-invalid @enderror" name="name"
-                                        value="{{ old('name', $plate->name) }}" required autocomplete="name"
-                                        placeholder="Inserisci il nome del piatto" autofocus>
-                                    @error('name')
+                        {{-- input descrizione piatto --}}
+                        <div class="form-group row">
+                            <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('Descrizione') }}
+                                <span class="text-warning">*</span></label>
+                            <div class="col-md-6">
+                                <textarea class="form-control @error('description') is-invalid @enderror" id="exampleFormControlTextarea1"
+                                    rows="3" name="description" required autocomplete="description" minlength="10"
+                                    placeholder="Inserisci la descrizione del piatto">{{ $plate->description }}</textarea>
+                                @error('description')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        {{-- input prezzo piatto --}}
+                        <div class="form-group row mt-3">
+                            <label for="price" class="col-md-4 col-form-label text-md-right">{{ __('Prezzo') }}
+                                <span class="text-warning">*</span></label>
+                            <div class="col-md-6">
+                                <input id="price" type="number" min="0.1" step=".01"
+                                    class="form-control @error('price') is-invalid @enderror" name="price"
+                                    value="{{ old('price', $plate->price) }}" required autocomplete="price"
+                                    placeholder="10.00">
+                                @error('price')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        {{-- select visibilità piatto --}}
+                        <div class="form-group row">
+                            <label for="visible" class="col-md-4 col-form-label text-md-right">{{ __('Visibile') }}
+                                <span class="text-warning">*</span></label>
+                            <div class="col-md-6">
+                                <select class="form-control @error('closing_day') is-invalid @enderror" name="visible"
+                                    required autocomplete="visible">
+                                    <option value="1" {{ $plate->visible == old('closing_day') ? 'selected' : '' }}>
+                                        Si</option>
+                                    <option value="0" {{ $plate->visible == old('closing_day') ? 'selected' : '' }}>
+                                        No</option>
+                                    @error('visible')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
-                                </div>
+                                </select>
                             </div>
+                        </div>
 
-                            {{-- input descrizione piatto --}}
-                            <div class="form-group row">
-                                <label for="description"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Descrizione') }}
-                                    <span class="text-warning">*</span></label>
-                                <div class="col-md-6">
-                                    <textarea class="form-control @error('description') is-invalid @enderror" id="exampleFormControlTextarea1"
-                                        rows="3" name="description" required autocomplete="description" minlength="10"
-                                        placeholder="Inserisci la descrizione del piatto">{{ $plate->description }}</textarea>
-                                    @error('description')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
+                        {{-- input immagine piatto --}}
+                        <div class="form-group row">
+                            <label for="image"
+                                class="col-md-4 col-form-label text-md-right">{{ __('Immagine ristorante') }}
+                                <span class="text-warning">*</span>
+                            </label>
+                            <div class="col-md-6">
+                                <input type="file" class="form-control-file" name="image">
                             </div>
+                            <div class="mt-3">
+                                <img class="w-50" src="{{ asset('storage/' . $plate->image) }}">
+                            </div>
+                        </div>
 
-                            {{-- input prezzo piatto --}}
-                            <div class="form-group row mt-3">
-                                <label for="price" class="col-md-4 col-form-label text-md-right">{{ __('Prezzo') }}
-                                    <span class="text-warning">*</span></label>
-                                <div class="col-md-6">
-                                    <input id="price" type="number" min="0.1" step=".01"
-                                        class="form-control @error('price') is-invalid @enderror" name="price"
-                                        value="{{ old('price', $plate->price) }}" required autocomplete="price"
-                                        placeholder="10.00">
-                                    @error('price')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
+                        {{-- bottone di submit dati --}}
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-warning">
+                                    {{ __('Modifica piatto') }}
+                                </button>
                             </div>
-
-                            {{-- select visibilità piatto --}}
-                            <div class="form-group row">
-                                <label for="visible" class="col-md-4 col-form-label text-md-right">{{ __('Visibile') }}
-                                    <span class="text-warning">*</span></label>
-                                <div class="col-md-6">
-                                    <select class="form-control @error('closing_day') is-invalid @enderror" name="visible"
-                                        required autocomplete="visible">
-                                        <option value="1"
-                                            {{ $plate->visible == old('closing_day') ? 'selected' : '' }}>Si</option>
-                                        <option value="0"
-                                            {{ $plate->visible == old('closing_day') ? 'selected' : '' }}>No</option>
-                                        @error('visible')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </select>
-                                </div>
-                            </div>
-
-                            {{-- input immagine piatto --}}
-                            <div class="form-group row">
-                                <label for="image"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Immagine ristorante') }}
-                                    <span class="text-warning">*</span>
-                                </label>
-                                <div class="col-md-6">
-                                    <input type="file" class="form-control-file" name="image">
-                                </div>
-                                <div class="mt-3">
-                                    <img class="w-50" src="{{ asset('storage/' . $plate->image) }}">
-                                </div>
-                            </div>
-
-                            {{-- bottone di submit dati --}}
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-warning">
-                                        {{ __('Modifica piatto') }}
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
