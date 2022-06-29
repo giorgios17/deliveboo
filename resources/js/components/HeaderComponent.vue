0d0ac0e1d3cd9161efe2384643c834ade7be384f
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-06-29 10:31:23 +0200

    modifiche all'HeaderComponent

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -12,14 +12,14 @@
       >
         <li class="nav-item dropdown">
           <a
-            class="nav-link dropdown-toggle"
+            class="nav-link"
             href="#"
             id="navbarDropdownMenuLink"
             role="button"
             data-bs-toggle="dropdown"
             aria-expanded="false"
           >
-            <a href=""><i class="fa-solid fa-bars"></i></a>
+            <i class="fa-solid fa-bars fa-xl"></i>
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
             <li><a class="dropdown-item" href="#">Home</a></li>
@@ -30,27 +30,35 @@
 
         <img class="w-50" src="../img/deliveboo-logo-green.png" alt="" />
 
-        <li></li>
-        <li class="col"><a href=""></a></li>
+        <li class="col-4 d-flex justify-content-center">
+          <a href="/admin"><i class="fa-solid fa-user fa-xl mx-3"></i></a>
 
-        <li class="col-3">
-          <a href=""><i class="fa-solid fa-user"></i></a>
-
-          <a href=""><i class="fa-solid fa-cart-shopping"></i></a>
+          <a href=""><i class="fa-solid fa-cart-shopping fa-xl"></i></a>
         </li>
       </ul>
     </div>
     <div class="d-none d-md-block d-lg-block">
-      <ul class="nav justify-content-center p-2 text-center align-items-center">
+      <ul
+        class="
+          nav
+          justify-content-evenly
+          p-2
+          text-center
+          align-items-center
+          nav_link
+        "
+      >
         <img class="col-2" src="../img/deliveboo-logo-green.png" alt="" />
 
-        <li class="col-2"><a href="">HOME</a></li>
+        <li class="col-2"><a href="/home">HOME</a></li>
         <li class="col-2"><a href="">CHI SIAMO</a></li>
         <li class="col-2"><a href="">LAVORA CON NOI</a></li>
-        <a class="col-1" href=""><i class="fa-solid fa-cart-shopping"></i></a>
+        <a class="col-1" href=""
+          ><i class="fa-solid fa-cart-shopping fa-xl"></i
+        ></a>
 
         <li class="col-2">
-          <a class="p-2" href="/admin"> Sei un ristoratore? REGISTRATI/LOGIN</a>
+          <a class="p-1" href="/admin"> Sei un ristoratore? REGISTRATI/LOGIN</a>
         </li>
       </ul>
     </div>
@@ -68,6 +76,7 @@ export default {
 
 header {
   background-color: $yellow;
+  font-weight: 700;
   ul {
     list-style-type: none;
   }
