47addb6441b62e1f50481090ee609042253f3d26
Davide Venturelli <dventurelli90@gmail.com>
2022-06-29 10:07:01 +0200

    modifica layout TypolyCard + layout HomepageComponent

--- resources/js/components/TypologyCard.vue
+++ resources/js/components/TypologyCard.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
-    <div class="container">
-      <div class="d-flex flex-wrap">
-        <div v-for="(typology, index) in arrayTypologies" :key="index">
-          <img :src="typology.image" :alt="typology.name" />
-          <h1>{{ typology.name }}</h1>
+    <div class="container my-5">
+      <div class="row">
+        <div
+          class="
+            col-lg-3 col-md-4 col-sm-6 col-12
+            text-center
+            my-3
+            typology_card
+          "
+          v-for="(typology, index) in arrayTypologies"
+          :key="index"
+        >
+          <a href="/">
+            <img :src="typology.image" :alt="typology.name" />
+            <h4>{{ typology.name }}</h4>
+          </a>
         </div>
       </div>
     </div>
@@ -25,5 +36,15 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+img {
+  width: 100%;
+  border-radius: 10px;
+}
+a:hover {
+  text-decoration: none;
+}
+.typology_card:hover {
+  transform: scale(1.05);
+}
 </style>
--- resources/js/pages/HomepageComponent.vue
+++ resources/js/pages/HomepageComponent.vue
@@ -1,6 +1,85 @@
 <template>
   <div>
+    <!--Jumbotron-->
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12 d-flex justify-content-center align-items-center jb">
+          <h1>DELIVEBOO</h1>
+        </div>
+      </div>
+    </div>
+
+    <!--Sezione tipologie ristoranti-->
+    <div class="container my-5">
+      <div class="row">
+        <div class="col-12">
+          <h3>Le cucine più amate</h3>
+          <p>
+            Trova le cucine più amate dei nostri ristoranti e ordina online a
+            domicilio
+          </p>
+        </div>
+      </div>
+
+      <div class="container mt-5">
+        <div class="row justify-content-around flex-wrap">
+          <div class="col-md-3 col-sm-3 col-12 text-center card p-2 mt-2 bg">
+            <p>Scegli i tui piatti dal menù!</p>
+          </div>
+          <div class="col-md-3 col-sm-3 col-12 text-center card p-2 mt-2 bg">
+            Ordina da tuo ristorante preferito.
+          </div>
+          <div class="col-md-3 col-sm-3 col-12 text-center card p-2 mt-2 bg">
+            Prepara il tavolo, arriviamo!
+          </div>
+        </div>
+      </div>
+    </div>
+
     <TypologyCard :arrayTypologies="arrayTypologies" />
+
+    <!--Sezione recensioni-->
+    <div class="container-fluid py-5 text-center bg">
+      <h1>SLIDER RECENSIONI</h1>
+    </div>
+
+    <!--Sezione download-->
+    <div class="container my-5">
+      <div class="row justify-content-between">
+        <div class="col-12 col-md-5 px-5">
+          <h3 class="mb-3">SCARICA LA NOSTRA APP!</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime
+            tempora veniam tenetur explicabo blanditiis! Officia nostrum quam
+            enim mollitia, iusto qui quae ad voluptatem possimus id sint eius!
+            Voluptatibus, similique.
+          </p>
+          <div class="row">
+            <div class="col-6 d-flex justify-content-center py-5">
+              <img
+                class="download_logo"
+                src="../img/App_Store_Badge.svg"
+                alt="app store"
+              />
+            </div>
+            <div class="col-6 d-flex justify-content-center py-5">
+              <img
+                class="download_logo"
+                src="../img/Google_Play_Store_badge.svg"
+                alt="play store"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="col-5 d-none d-md-block d-lg-block">
+          <img
+            class="download_cover"
+            src="../img/download-cover.png"
+            alt="download"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,5 +114,29 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.jb {
+  background-image: url(../img/background-hp.jpg);
+  background-position: bottom;
+  background-size: cover;
+  h1 {
+    font-size: 60px;
+    font-weight: 800;
+    padding: 350px 0;
+  }
+}
+.bg {
+  background-color: #003049;
+  color: hsl(53deg 37% 77%);
+  //background-color: #fcbf49;
+  //color: #003049;
+}
+.download_logo {
+  width: 80%;
+}
+.download_cover {
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+}
 </style>
