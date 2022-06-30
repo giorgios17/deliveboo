c062a0d587311a2114ad5a9174c268f7c55a29cc
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-30 22:18:50 +0200

    stilizzato layout pagina not found

--- resources/views/404/notFound.blade.php
+++ resources/views/404/notFound.blade.php
@@ -1,15 +1,21 @@
 @extends('layouts.appAdmin')
 @section('content')
-    <div class="container">
+    <div id="not_found" class="container my-5">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('404 Not Found') }}</div>
-
-                    <div class="card-body">
-                        <p>The page you are looking for does not exist.</p>
+                    <div class="card-header">
+                        <h5 class="card-title">{{ __('404 Not Found') }}</h5>
                     </div>
-                    <a class="btn btn-primary" href="/">Torna alla Home</a>
+                    <div class="card-body">
+                        <p class="card-text">
+                            {{-- box alert pagina non trovata --}}
+                        <div class="alert alert-danger" role="alert">
+                            La pagina che stai cercando non esiste.
+                        </div>
+                        </p>
+                        <a href="admin/user" class="btn btn-danger">Torna alla Home</a>
+                    </div>
                 </div>
             </div>
         </div>
