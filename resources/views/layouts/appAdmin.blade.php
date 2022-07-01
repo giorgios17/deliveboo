e71b7990184800a683148a38ca5a59dfa8c95e14
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-07-01 15:49:34 +0200

    rilavorata navbar

--- resources/views/layouts/appAdmin.blade.php
+++ resources/views/layouts/appAdmin.blade.php
@@ -32,69 +32,40 @@
 
 <body>
     <div id="app" class="layout_base">
-        <div class="position-fixed w-100">
-            <nav class="navbar navbar-expand-md shadow-sm">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        <div class="wrapper_logo">
-                            <img class="w-100 p-2" src="{{ asset('images/deliveboo-logo-blue.svg') }}" alt="">
-                        </div>
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                        {{-- <span class="navbar-toggler-icon"> <i class="fa-solid fa-bars"></i></span> --}}
-                        <div class="navbar-toggler-icon d-flex align-items-center justify-content-center"> <i
-                                class="fa-solid fa-bars"></i></div>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="navbar-nav mr-auto">
-
-                        </ul>
-
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto">
-                            <!-- Authentication Links -->
-                            @guest
-                                <li class="nav-item">
-
-                                    <a class="nav-link font-weight-bold" href="{{ route('login') }}"><i
-                                            class="fa-solid fa-arrow-right-to-bracket"></i> {{ __('Accedi') }} </a>
-                                </li>
-                                @if (Route::has('register'))
-                                    <li class="nav-item">
-                                        <a class="nav-link font-weight-bold" href="{{ route('register') }}">
-                                            <i class="fa-solid fa-pen-to-square"></i> {{ __('Registrati') }}</a>
-                                    </li>
-                                @endif
-                            @else
-                                <li class="nav-item dropdown">
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        {{ Auth::user()->name }}
-                                    </a>
-
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                            class="d-none">
-                                            @csrf
-                                        </form>
-                                    </div>
-                                </li>
-                            @endguest
-                        </ul>
+        <nav class="navbar navbar-expand-md shadow-sm">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <div class="wrapper_logo">
+                        <img class="w-100 p-2" src="{{ asset('images/deliveboo-logo-blue.svg') }}" alt="">
                     </div>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
+                    {{-- <span class="navbar-toggler-icon"> <i class="fa-solid fa-bars"></i></span> --}}
+                    <div class="navbar-toggler-icon d-flex align-items-center justify-content-center"> <i
+                            class="fa-solid fa-bars"></i></div>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        <li class="nav-item">
+
+                            <a class="nav-link font-weight-bold" href="{{ route('login') }}"><i
+                                    class="fa-solid fa-arrow-right-to-bracket"></i> {{ __('Accedi') }} </a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link font-weight-bold" href="{{ route('register') }}">
+                                    <i class="fa-solid fa-pen-to-square"></i> {{ __('Registrati') }}</a>
+                            </li>
+                        @endif
+                    </ul>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
 
         <main>
             @yield('content')
