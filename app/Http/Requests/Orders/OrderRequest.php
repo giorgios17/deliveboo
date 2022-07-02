dfeef51860add0d85cdfbbbd65d9f12e7acf1575
emicif <96519927+emicif@users.noreply.github.com>
2022-07-02 15:56:36 +0200

    creato valid plate e importato

--- app/Http/Requests/Orders/OrderRequest.php
+++ app/Http/Requests/Orders/OrderRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Orders;
 
+use App\Rules\ValidPlate;
 use Illuminate\Foundation\Http\FormRequest;
 
 class OrderRequest extends FormRequest
@@ -25,7 +26,10 @@ class OrderRequest extends FormRequest
     {
         return [
             'token' => 'required',
-            'amount' =>'required'
+            'plate' => [
+                'required',
+                new ValidPlate()
+            ]
         ];
     }
 }
