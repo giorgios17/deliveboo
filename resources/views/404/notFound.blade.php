1bcbe9aed16a93fc17a84163b5c0752b036a6b64
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-07-02 14:51:43 +0200

    separate pagine 404 utente e ospite

--- resources/views/404/notFound.blade.php
+++ resources/views/404/notFound.blade.php
@@ -3,20 +3,37 @@
     <div id="not_found" class="container my-5">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title">{{ __('404 Not Found') }}</h5>
-                    </div>
-                    <div class="card-body">
-                        <p class="card-text">
-                            {{-- box alert pagina non trovata --}}
-                        <div class="alert alert-danger" role="alert">
-                            La pagina che stai cercando non esiste.
+                @guest
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">{{ __('404 Not Found') }}</h5>
                         </div>
-                        </p>
-                        <a href="/" class="btn btn-danger">Torna alla Home</a>
+                        <div class="card-body">
+                            <p class="card-text">
+                                {{-- box alert pagina non trovata --}}
+                            <div class="alert alert-danger" role="alert">
+                                La pagina che stai cercando non esiste.
+                            </div>
+                            </p>
+                            <a href="/" class="btn">Torna alla Home</a>
+                        </div>
                     </div>
-                </div>
+                @else
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">{{ __('404 Not Found') }}</h5>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">
+                                {{-- box alert pagina non trovata --}}
+                            <div class="alert alert-danger" role="alert">
+                                La pagina che stai cercando non esiste.
+                            </div>
+                            </p>
+                            <a href="/admin/user" class="btn">Torna alla Dashboard</a>
+                        </div>
+                    </div>
+                @endguest
             </div>
         </div>
     @endsection
