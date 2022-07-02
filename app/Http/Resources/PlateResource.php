a0b91952b675bb6a2bb64d7298c77e8928ce125a
emicif <96519927+emicif@users.noreply.github.com>
2022-07-02 15:38:44 +0200

    aggiornato plateresource

--- app/Http/Resources/PlateResource.php
+++ app/Http/Resources/PlateResource.php
@@ -15,7 +15,14 @@ class PlateResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'price' => $this->price,
+            'visible' => $this->visible,
+            'image' => $this->image,
+            'user_id' => $this->user_id,
+
         ];
     }
 }
