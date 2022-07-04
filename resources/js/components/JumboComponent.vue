81de40bc3761b23139e831219ab4d04e88e89b30
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-07-04 19:34:31 +0200

    creato componente jumbo e incollato codice dalla homepage

---
+++ resources/js/components/JumboComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <!--Jumbotron-->
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 d-flex justify-content-center align-items-center jb">
+        <h1 class="h1_jumbo">DELIVEBOO</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "JumboComponent",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "/resources/sass/_variables";
+@import "/resources/sass/_mixin";
+
+.jb {
+  background-image: url("../img/jumbotron.png");
+  background-position: top;
+  background-size: cover;
+  background-blend-mode: multiply;
+  background-color: rgba(0, 0, 0, 0.2);
+  h1.h1_jumbo {
+    font-size: 60px;
+    font-weight: 900;
+    color: $blue;
+    padding: 350px 0;
+  }
+  @media screen and (max-width: 768px) {
+    h1.h1_jumbo {
+      font-size: 40px;
+      padding: 200px 0;
+    }
+  }
+}
+</style>
