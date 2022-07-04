77badbeb254a0262f4f899edbd3dc7cea6b32bea
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-07-04 20:39:37 +0200

    rimodellato layout

--- resources/js/components/CarouselComponent.vue
+++ resources/js/components/CarouselComponent.vue
@@ -1,23 +1,29 @@
 <template>
   <!-- carousel -->
-  <div class="container-fluid bg py-3">
-    <div class="row">
+  <div class="container-fluid bg py-5">
+    <div class="row justify-content-center mb-5">
+      <div class="wrapper_img">
+        <img class="w-100" :src="'/storage' + review" />
+      </div>
+    </div>
+    <div class="row justify-content-center">
       <!-- Controllo sx -->
       <div
-        class="col-2 d-flex justify-content-center align-items-center prev"
+        class="col-md-2 col-sm-4 text-center prev mb-4"
         @click="toggleImg('prev')"
       >
-        <i class="fa-solid fa-4x fa-circle-chevron-left"></i>
-      </div>
-      <div class="col-8 d-flex justify-content-center">
-        <img :src="'/storage' + review" />
+        <i class="fa-solid fa-2x fa-circle-chevron-left">
+          <p class="mt-2">Precedente</p>
+        </i>
       </div>
       <!-- Controllo dx -->
       <div
-        class="col-2 d-flex justify-content-center align-items-center next"
+        class="col-md-2 col-sm-4 text-center next"
         @click="toggleImg('next')"
       >
-        <i class="fa-solid fa-4x fa-circle-chevron-right"></i>
+        <i class="fa-solid fa-2x fa-circle-chevron-right">
+          <p class="mt-2">Successivo</p>
+        </i>
       </div>
     </div>
   </div>
@@ -68,12 +74,55 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "/resources/sass/_resets";
+@import "/resources/sass/_variables";
+@import "/resources/sass/_mixin";
+
+.bg {
+  background-color: $blue;
+}
+
+.wrapper_img {
+  width: 250px;
+}
+
 .fa-circle-chevron-left,
 .fa-circle-chevron-right {
+  color: $tortora;
   cursor: pointer;
   transition: transform 0.1s linear;
   &:hover {
     transform: scale(1.2);
+  }
+}
+
+p {
+  @include p($tortora);
+}
+
+@media screen and (max-width: 300px) {
+  .wrapper_img {
+    width: 150px;
+  }
+  .fa-circle-chevron-left,
+  .fa-circle-chevron-right {
+    font-size: 1.25rem;
+  }
+  p.mt-2 {
+    font-size: 0.75rem;
+  }
+}
+
+@media screen and (max-width: 200px) {
+  .wrapper_img {
+    width: 100px;
+  }
+  .fa-circle-chevron-left,
+  .fa-circle-chevron-right {
+    font-size: 1rem;
+  }
+  p.mt-2 {
+    font-size: 0.5rem;
   }
 }
 </style>
