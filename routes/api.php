5a573855679935d27f2ddd94f93c8f29749410f3
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-07-04 16:51:27 +0200

    rimossa api

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,6 @@ Route::get('plates', 'Api\PlateController@index');
 Route::get('typologies', 'Api\TypologyController@index');
 Route::get('/users/{slug}', 'Api\RestaurantsController@getPlates');
 Route::get('/restaurants/filter/{typologies}', 'Api\RestaurantsController@filterRestaurants');
-Route::get('users', 'Api\UserController@index');
 
 // rotte per la gestione dei pagamenti
 Route::get("orders/generate", "Api\Orders\OrderController@generate");
