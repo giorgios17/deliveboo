6e53dd616bbdae6dc8b097131d3356a9915c06d1
Davide Venturelli <dventurelli90@gmail.com>
2022-07-04 08:33:27 +0200

    modifica link Homepage

--- resources/js/pages/HomepageComponent.vue
+++ resources/js/pages/HomepageComponent.vue
@@ -50,19 +50,26 @@
             comodamente a casa tua! - disponibile su Apple store e Google Play
           </p>
           <div class="row">
-            <div class="col-6 d-flex justify-content-center py-5">
-              <img
-                class="download_logo"
-                src="../img/App_Store_Badge.svg"
-                alt="app store"
-              />
+            <div class="col-12 col-md-6 d-flex justify-content-center py-5">
+              <a
+                href="https://apps.apple.com/it/app/deliveroo-consegna-di-cibo/id1001501844"
+                ><img
+                  class="download_logo"
+                  src="../img/App_Store_Badge.svg"
+                  alt="app store"
+                />
+              </a>
             </div>
-            <div class="col-6 d-flex justify-content-center py-5">
-              <img
-                class="download_logo"
-                src="../img/Google_Play_Store_badge.svg"
-                alt="play store"
-              />
+            <div class="col-12 col-md-6 d-flex justify-content-center py-5">
+              <a
+                href="https://play.google.com/store/apps/details?id=com.deliveroo.orderapp&hl=it&gl=US"
+              >
+                <img
+                  class="download_logo"
+                  src="../img/Google_Play_Store_badge.svg"
+                  alt="play store"
+                />
+              </a>
             </div>
           </div>
         </div>
@@ -127,7 +134,13 @@ export default {
   color: hsl(53deg 37% 77%);
 }
 .download_logo {
-  width: 80%;
+  width: 100%;
+}
+a {
+  transition: all 0.5s;
+}
+a:hover {
+  transform: scale(1.1);
 }
 .download_cover {
   width: 100%;
