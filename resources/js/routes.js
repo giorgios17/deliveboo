21d3cb23b4c60474abac3787afb6bca2e0264bfa
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-07-05 18:02:05 +0200

    aggiunta rotta carrello

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -3,6 +3,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import HomepageComponent from './pages/HomepageComponent';
 import RestaurantComponent from './pages/RestaurantComponent';
+import CartComponent from './pages/CartComponent';
 import NotFound from './pages/NotFound';
 Vue.use(VueRouter);
 
@@ -18,6 +19,11 @@ const router = new VueRouter({
             path: '/restaurant/:slug',
             name: 'restaurant',
             component: RestaurantComponent
+        },
+        {
+            path: '/cart',
+            name: 'cart',
+            component: CartComponent
         },
         {
             path: '/*',
