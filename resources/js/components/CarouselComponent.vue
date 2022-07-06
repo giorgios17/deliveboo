daaba6572d9b43e0b3e2217e90293358c927878c
giorgios17 <giosur@live.it>
2022-07-06 15:45:02 +0200

    fix col carosello

--- resources/js/components/CarouselComponent.vue
+++ resources/js/components/CarouselComponent.vue
@@ -4,7 +4,7 @@
     <div class="row justify-content-center align-items-center">
       <!-- Controllo sx -->
       <div
-        class="col-md-2 col-sm-4 text-center prev"
+        class="col-md-1 col-sm-2 text-center prev"
         @click="toggleImg('prev')"
       >
         <i class="fa-solid fa-2x fa-circle-chevron-left"> </i>
@@ -14,7 +14,7 @@
       </div>
       <!-- Controllo dx -->
       <div
-        class="col-md-2 col-sm-4 text-center next"
+        class="col-md-2 col-sm-2 text-center next"
         @click="toggleImg('next')"
       >
         <i class="fa-solid fa-2x fa-circle-chevron-right"> </i>
