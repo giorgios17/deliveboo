6412f2c5a12e5789cfdb6ed4215ca59ff9d53857
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-07-06 10:20:41 +0200

    aggiunti piatti

--- config/plates.php
+++ config/plates.php
@@ -73,4 +73,228 @@ return [
         "image" => "images/plates/uramaki.jpg",
         "user_id" => "3"
     ],
+    [
+        "name"  => "Beef steak",
+        "description" => "Tagliata di manzo su letto di rucola",
+        "price" => "15.00",
+        "visible" => "1",
+        "image" => "images/plates/beef-steak.jpg",
+        "user_id" => "4"
+    ],
+    [
+        "name"  => "New York strip",
+        "description" => "Bistecca di controfiletto di manzo",
+        "price" => "18.00",
+        "visible" => "1",
+        "image" => "images/plates/ny-strip.jpg",
+        "user_id" => "4"
+    ],
+    [
+        "name"  => "Bbq ribs",
+        "description" => "Costine di maialino da latte cotte a bassa temperatura e caramellata in salsa BBQ",
+        "price" => "13.00",
+        "visible" => "1",
+        "image" => "images/plates/bbq-ribs.jpg",
+        "user_id" => "4"
+    ],
+    [
+        "name"  => "Hamburger di manzo con salsa barbecue",
+        "description" => "Gustoso hamburger di manzo tritato a cui è data la forma di un disco schiacciato, ben cotta fuori e al sangue dentro, che ben si sposa con insalate, pomodori e patatine.",
+        "price" => "16.00",
+        "visible" => "1",
+        "image" => "images/plates/hamburger-di-manzo.jpg",
+        "user_id" => "4"
+    ],
+    [
+        "name"  => "Piadine con feta greca",
+        "description" => "Una fresca bontà d’ispirazione greca.",
+        "price" => "9.00",
+        "visible" => "1",
+        "image" => "images/plates/piadina-greca.jpg",
+        "user_id" => "5"
+    ],
+    [
+        "name"  => "Baguette al pomodoro e olive",
+        "description" => "Saporiti e leggeri, questi filoncini sono l’ideale sia come stuzzichini per le feste sia per un rapido spuntino!",
+        "price" => "7.50",
+        "visible" => "1",
+        "image" => "images/plates/baguette-pomodoro-olive.jpg",
+        "user_id" => "5"
+    ],
+    [
+        "name"  => "Polpette di broccoli Bio",
+        "description" => "Pochi ingredienti semplici, i broccoli e i nostri Panini BIO delle polpette di broccoli che vi stupiranno per aspetto appetitoso e sapore IRRESISTIBILE!",
+        "price" => "6.00",
+        "visible" => "1",
+        "image" => "images/plates/polpette-broccoli.jpg",
+        "user_id" => "5"
+    ],
+    [
+        "name"  => "Involtini primavera",
+        "description" => "Piatto ripieno, con sfoglia sottile e croccante e farciti con un mix di verdure dal sapore inconfondibile.",
+        "price" => "5.00",
+        "visible" => "1",
+        "image" => "images/plates/involtini-primavera.jpg",
+        "user_id" => "6"
+    ],
+    [
+        "name"  => "Riso alla cantonese",
+        "description" => "Uno dei piatti a base di riso più famoso di questa cucina. Piselli, tocchetti di prosciutto cotto, uova insaporiscono bianchi chicchi sodi e gustosi.",
+        "price" => "7.00",
+        "visible" => "1",
+        "image" => "images/plates/riso-cantonese.jpg",
+        "user_id" => "6"
+    ],
+    [
+        "name"  => "Pollo alle mandorle",
+        "description" => "Piccoli bocconcini di tenero pollo conditi con una gustosa crema a base di salsa di soia e mandorle.",
+        "price" => "4.00",
+        "visible" => "1",
+        "image" => "images/plates/pollo-mandorle.jpg",
+        "user_id" => "6"
+    ],
+    [
+        "name"  => "Tonno tartare",
+        "description" => "Tartare di tonno fresco accompagnato de un contorno di avocado.",
+        "price" => "5.00",
+        "visible" => "1",
+        "image" => "images/plates/tonno-tartare.jpg",
+        "user_id" => "7"
+    ],
+    [
+        "name"  => "salmone tartare",
+        "description" => "Tartare di salmone fresco accompagnato de un contorno di avocado.",
+        "price" => "5.00",
+        "visible" => "1",
+        "image" => "images/plates/salmone-tartare.jpg",
+        "user_id" => "7"
+    ],
+    [
+        "name"  => "Gyosa",
+        "description" => "Ravioli giapponesi con carne e verdure",
+        "price" => "4.50",
+        "visible" => "1",
+        "image" => "images/plates/gyosa.jpg",
+        "user_id" => "7"
+    ],
+    [
+        "name"  => "Gelato alla frutta",
+        "description" => "Gelato alla frutta artigianale",
+        "price" => "3.00",
+        "visible" => "1",
+        "image" => "images/plates/gelato-alla-frutta.jpg",
+        "user_id" => "8"
+    ],
+    [
+        "name"  => "Fragola e limone",
+        "description" => "Perfetto per una sana, nutriente e rinfrescante merenda",
+        "price" => "2.50",
+        "visible" => "1",
+        "image" => "images/plates/gelato-fragola.jpg",
+        "user_id" => "8"
+    ],
+    [
+        "name"  => "Cioccolato e nocciola",
+        "description" => "Un connubio tra la bontà del gelato alla nocciola e la classicità di quello al cioccolato",
+        "price" => "2.00",
+        "visible" => "1",
+        "image" => "images/plates/gelato-al-cioccolato.jpg",
+        "user_id" => "8"
+    ],
+    [
+        "name"  => "Salmon pokè",
+        "description" => "Riso sushi, salmone marinato con salsa teriyaki e miele. julienne di iceberg, alga wakame, alghe nori, mango, more, pomodorini, sesamo nero, salsa teriyaki.",
+        "price" => "14.00",
+        "visible" => "1",
+        "image" => "images/plates/salmone-poke.jpg",
+        "user_id" => "9"
+    ],
+    [
+        "name"  => "Gambero rosso pokè",
+        "description" => "Riso sushi, gamberi rossi crudi con salsa agli agrumi, misticanza, cappuccio viola, ananas, edamame, perle di riso, salsa agrumi.",
+        "price" => "15.00",
+        "visible" => "1",
+        "image" => "images/plates/gambero-rosso-poke.jpg",
+        "user_id" => "9"
+    ],
+    [
+        "name"  => "Vegan pokè",
+        "description" => "Riso sushi, rucola, avocado, mango, pomodorini, carote allo zenzero, edamame, daikon, olio di oliva.",
+        "price" => "12.00",
+        "visible" => "1",
+        "image" => "images/plates/vegan-poke.jpg",
+        "user_id" => "9"
+    ],
+    [
+        "name"  => "Polpettine di legumi al ragù",
+        "description" => "Seitan, falanghina del beneventano Doc, carpaccio di mopur, salvia e olio extravergine d’oliva.",
+        "price" => "7.00",
+        "visible" => "1",
+        "image" => "images/plates/polpettine-legumi-ragu.jpg",
+        "user_id" => "10"
+    ],
+    [
+        "name"  => "Panino Cavoli Nostri Burger",
+        "description" => "Burger di quinoa, fagioli rossi, olive, carpaccio di mopur, coleslaw, insalata mista e pomodori.",
+        "price" => "9.00",
+        "visible" => "1",
+        "image" => "images/plates/burger-cavoli.jpg",
+        "user_id" => "10"
+    ],
+    [
+        "name"  => "Tiramisù",
+        "description" => "Latte di soia, zucchero, panna vegetale, cacao, caffè, biscotti e maraschino.",
+        "price" => "6.00",
+        "visible" => "1",
+        "image" => "images/plates/tiramisu.jpg",
+        "user_id" => "10"
+    ],
+    [
+        "name"  => "Rogan Josh, kashmir",
+        "description" => "Carne di lombo, un sughetto piccante eccezionale e il classico pane indiano; esiste solo nella versione classica del Kashmir non adatta ai vegetariani. ”.",
+        "price" => "11.00",
+        "visible" => "1",
+        "image" => "images/plates/rogan-josh.jpg",
+        "user_id" => "11"
+    ],
+    [
+        "name"  => "Undiyo, gujarat",
+        "description" => "Si mangia con puri appena sfornati (le piccole focaccine indiane di cui c'è anche una versione con lenticchie) o roti. Si tratta di un mix di verdure in una salsa di erbe e spezie speciale. E' un piatto invernale nel Gujarat. ”.",
+        "price" => "12.00",
+        "visible" => "1",
+        "image" => "images/plates/undhiyo.jpg",
+        "user_id" => "11"
+    ],
+    [
+        "name"  => "Puran poli",
+        "description" => "Questa speciale focaccia è una focaccia dolce cotta nel classico tandoori. E' di solito servita come piatto principale accompagnando riso e verdure. Può essere consumata anche come dessert. ”.",
+        "price" => "6.00",
+        "visible" => "1",
+        "image" => "images/plates/puranpoli.jpg",
+        "user_id" => "11"
+    ],
+    [
+        "name"  => "Pozole",
+        "description" => "Stufato a base di mais cotto servito con carne, cavolo affettato, peperoncino, cipolla, aglio e altri aromi e spezie..",
+        "price" => "7.00",
+        "visible" => "1",
+        "image" => "images/plates/pozole.jpg",
+        "user_id" => "12"
+    ],
+    [
+        "name"  => "Chapulines",
+        "description" => "Le Chapulines sono cavallette tostate alla piastra e condite con aglio, succo di lime e sale. Croccanti e gustose, possono essere una vera scoperta se si ha il coraggio di assaggiarle..",
+        "price" => "3.00",
+        "visible" => "1",
+        "image" => "images/plates/chapulines.jpg",
+        "user_id" => "12"
+    ],
+    [
+        "name"  => "Gorditas",
+        "description" => "Latte di soia, zucchero, panna vegetale, cacao, caffè, biscotti e maraschino.",
+        "price" => "6.00",
+        "visible" => "1",
+        "image" => "images/plates/gorditas.jpg",
+        "user_id" => "12"
+    ],
 ];
