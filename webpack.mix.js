eff2184bcc451df63156a43d69c89cfe2509df92
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-07-07 19:54:26 +0200

    aggiunto alerts js

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/deliveboo.js', 'public/js')
     .js('resources/js/backoffice.js', 'public/js')
+    .js('resources/js/alerts.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
