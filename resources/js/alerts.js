ddb271a818ef93bf06ffc1def695b5dac0d3506d
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-07-07 19:54:42 +0200

    aggiunto file alerts nella cartella resources

---
+++ resources/js/alerts.js
@@ -0,0 +1,44 @@
+let form = document.querySelector('#delete_form'); // <-- verifico che l'elemento non abbia valore nullo salvando il form in una variabile
+
+if (form) { // <-- se il form esiste e quindi non è nullo
+
+    form.addEventListener('submit', function (e) { // <-- aggiungo un evento submit al form
+
+        e.preventDefault(); // <-- prevengo il comportamento di default del form
+
+        const swalWithBootstrapButtons = Swal.mixin({ // <-- creo una variabile che mi permette di utilizzare la funzione di sweetalert con i bottoni di bootstrap
+            customClass: {
+                confirmButton: 'btn btn-success',
+                cancelButton: 'btn btn-danger'
+            },
+            buttonsStyling: false
+        })
+
+        swalWithBootstrapButtons.fire({
+            title: 'Sei sicuro?',
+            text: "Se confermi non potrai ritornare indietro!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Si, sono sicuro!',
+            cancelButtonText: 'No, annulla!',
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) { // <-- se l'utente conferma l'eliminazione del piatto faccio partire il form di eliminazione
+                swalWithBootstrapButtons.fire(
+                    'Eliminato!',
+                    'Il tuo piatto è stato eliminato con successo!!',
+                    'success'
+                ).then(function () {
+                    form.submit(); // <-- faccio partire il form di eliminazione
+                });
+            } else { // <-- se l'utente annulla l'eliminazione del piatto non faccio partire il form di eliminazione
+                swalWithBootstrapButtons.fire(
+                    'Annullato',
+                    'Il piatto non è stato eliminato :)',
+                    'error'
+                )
+            }
+        })
+    });
+
+}
