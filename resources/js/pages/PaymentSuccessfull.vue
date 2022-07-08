bb3eb0441ab3dc40bdafe0a81b56dd61cebd88d6
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-07-08 12:08:00 +0200

    aggiunta pagina di succes

---
+++ resources/js/pages/PaymentSuccessfull.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>FRRRRRAAAAAAAAAAAAAAAAAAATTTTTTTTTTTTTMMMMMMMMMMMMMMMMMMMMMM</div>
+</template>
+
+<script>
+export default {
+  name: "PaymentSuccessfull",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
