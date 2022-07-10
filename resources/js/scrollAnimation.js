d1f607151bc4db7866ea67eb7d6d26e7cb338f78
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-07-10 18:34:16 +0200

    rimosso carattere

--- resources/js/scrollAnimation.js
+++ resources/js/scrollAnimation.js
@@ -1,5 +1,5 @@
 const animatedScrollObserver = new IntersectionObserver((entries, animatedScrollObserver) => {
-    entries.forEach((entry,) => {
+    entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('enter');
             animatedScrollObserver.unobserve(entry.target);
