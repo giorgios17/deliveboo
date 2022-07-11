495dc2568762f6a81e02e3cf3600f94d24bfda1e
giorgios17 <giosur@live.it>
2022-07-11 09:06:36 +0200

    fix layout

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -23,7 +23,7 @@
 
     </div>
 
-    <div id="user_index" class=" justify-content-center mb-5">
+    <div id="user_index" class="row flex-column justify-content-center mb-5">
         <h1 class="col-12 text-center p-3 mb-5 shadow">
             I TUOI ORDINI
         </h1>
@@ -35,11 +35,13 @@
                     <div class="card-body row">
                         <div class="col-md-6">
                             <h4>Ordine n. {{ $order->id }}</h4>
-                            <p class="card-title">{{ $order->customer_name }} {{ $order->customer_surname }}</p>
-                            <p class="card-title">{{ $order->customer_address }}</p>
+                            <hr>
+                            <p class="card-title">Nome e cognome cliente: {{ $order->customer_name }}
+                                {{ $order->customer_surname }}</p>
+                            <p class="card-title">Indirizzo: {{ $order->customer_address }}</p>
 
-                            <p class="card-title">{{ $order->customer_phone }}</p>
-                            <p class="card-title">{{ $order->customer_email }}</p>
+                            <p class="card-title">Telefono: {{ $order->customer_phone }}</p>
+                            <p class="card-title">Email: {{ $order->customer_email }}</p>
                             <p class="card-title">Totale: €{{ $order->total_price }}</p>
 
                             @if ($order->customer_note)
@@ -50,14 +52,20 @@
                         </div>
                         <div class="col-md-6">
                             <h4 class="mt-md-0 mt-3">Piatti ordinati</h4>
+                            <hr>
                             {{-- foreach piatti ordinati --}}
-                            @foreach ($order->plate as $plate)
-                                <p class="card-title">n. {{ $plate->pivot->quantity }} {{ $plate->name }} </p>
-                                <div class="md-0">
-                                    <img src="{{ asset('storage/' . $plate->image) }}" class="card-img-top h-100 w-100"
-                                        alt="{{ $plate->name }}">
-                                </div>
-                            @endforeach
+                            <div>
+                                @foreach ($order->plate as $plate)
+                                    <p class="card-title">{{ $plate->pivot->quantity }}x {{ $plate->name }} -
+                                        €{{ $plate->pivot->quantity * $plate->price }}</p>
+                                    <div class="md-0">
+                                        {{-- <div class="wrapper_img">
+                                        <img src="{{ asset('storage/' . $plate->image) }}"
+                                            class="card-img-top h-100 w-100" alt="{{ $plate->name }}">
+                                    </div> --}}
+                                    </div>
+                                @endforeach
+                            </div>
                         </div>
 
 
