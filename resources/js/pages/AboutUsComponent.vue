121ce6d0eaa67ab36e8c80c1e4cf1f70422f7c0b
Davide Venturelli <dventurelli90@gmail.com>
2022-07-11 16:33:23 +0200

    modifica layout about us

--- resources/js/pages/AboutUsComponent.vue
+++ resources/js/pages/AboutUsComponent.vue
@@ -1,170 +1,116 @@
 <template>
-  <div class="bg">
-    <div class="p-5 bg-primary">
-      <h1 class="title font-weight-bold">Chi siamo</h1>
-    </div>
-    <div class="container-fluid px-5 py-5">
+  <div>
+    <div class="container-fluid pt-5 text-center bg-blue">
       <div class="row">
-        <div class="col-12 col-md-4 mb-5">
-          <ul class="list-group shadow">
-            <li class="list-group-item">
-              <a class="font-weight-bold" href="#story"
-                ><i class="fa-solid fa-md fa-arrow-right-long mr-2"></i> Storia
-                di Deliveboo</a
-              >
-            </li>
-            <li class="list-group-item">
-              <a class="font-weight-bold" href="#client"
-                ><i class="fa-solid fa-md fa-arrow-right-long mr-2"></i>
-                Clienti</a
-              >
-            </li>
-            <li class="list-group-item">
-              <a class="font-weight-bold" href="#food"
-                ><i class="fa-solid fa-md fa-arrow-right-long mr-2"></i>
-                Ristoranti, negozi di alimentari e supermercati</a
-              >
-            </li>
-            <li class="list-group-item">
-              <a class="font-weight-bold" href="#rider"
-                ><i class="fa-solid fa-md fa-arrow-right-long mr-2"></i>
-                Rider</a
-              >
-            </li>
-            <li class="list-group-item">
-              <a class="font-weight-bold" href="#technology"
-                ><i class="fa-solid fa-md fa-arrow-right-long mr-2"></i> La
-                tecnologia ci guida</a
-              >
-            </li>
-            <li class="list-group-item">
-              <a class="font-weight-bold" href="#innovation"
-                ><i class="fa-solid fa-md fa-arrow-right-long mr-2"></i>
-                Innovazione a domicilio</a
-              >
-            </li>
-            <li class="list-group-item">
-              <a class="font-weight-bold" href="#values"
-                ><i class="fa-solid fa-md fa-arrow-right-long mr-2"></i> I
-                nostri valori</a
-              >
-            </li>
-          </ul>
+        <div class="col-12 mb-5">
+          <h1 class="font-weight-bold mb-3">Chi siamo</h1>
+          <p>
+            Deliveboo è una compagnia di consegna di cibo in rete fondata nel
+            2013 da Giorgio Surace. Con sede a Londra, in Inghilterra, opera in
+            più di 500 città in Regno Unito, Olanda, Francia, Belgio, Irlanda,
+            Spagna, Italia, Australia, Singapore, Emirati Arabi Uniti, Kuwait, e
+            Hong Kong.
+          </p>
         </div>
-        <div class="col-12 col-md-8 card px-5 shadow">
-          <!-- Storia -->
-          <h1 id="story" class="py-3 font-weight-bold">
-            La storia di Deliveboo
-          </h1>
-          <img src="../img/ceo.png" alt="immagine" />
-          <p class="py-3">
-            Quando il fondatore e CEO di Deliveboo Ciro Cusati si è trasferito a
-            Londra nel 2013, ha trovato una città piena di ristoranti
-            incredibili, ma è rimasto stupito che davvero pochi di questi
-            consegnassero a domicilio. Così, ha deciso che la sua missione
-            personale sarebbe stata quella di portare i migliori ristoranti
-            della città direttamente nelle case delle persone. Oggi Deliveboo è
-            al centro di un marketplace che mette in connessione tre attori: i
-            clienti, i ristoranti e i supermercati, e i rider. L’obiettivo
-            principale di questa connessione è facilitare un’esperienza di
-            acquisto indimenticabile, in circa 30 minuti. Offrendo una consegna
-            veloce e affidabile, che il cliente può seguire passo passo dallo
-            smartphone, Deliveboo ha visto una crescita rapida e sostenuta. Oggi
-            Deliveboo è presente in 11 mercati in tutto il mondo, collabora con
-            migliaia di ristoranti, supermercati e rider e arriva nelle case di
-            milioni di clienti.
-          </p>
-          <!-- Clienti -->
-          <h1 id="client" class="py-3 font-weight-bold">Clienti</h1>
-          <img src="../img/client.png" alt="immagine" />
-          <p class="py-3">
-            La consegna di cibo a domicilio è un’attività iperlocale: l’offerta
-            ai clienti è sviluppata in modo diverso da città a città e da
-            quartiere a quartiere. Da anni lavoriamo sodo per capire cosa
-            vogliono i clienti e per offrire una proposta adatta a loro sia a
-            livello geografico, sia individuale: dagli studenti ai
-            professionisti, passando per le famiglie, e dai centri delle città
-            ai paesi di periferia, grandi e piccoli. Tutti noi ci sediamo a
-            tavola e l’offerta di Deliveboo rispecchia i gusti delle persone
-            ovunque si trovino. Con la nostra proposta ai clienti cerchiamo di
-            offrire sempre la disponibilità, l’assortimento, l’esperienza e il
-            valore migliore possibile, creando nel frattempo un legame emotivo
-            con il nostro marchio.
-          </p>
-          <!-- Cibo -->
-          <h1 id="food" class="py-3 font-weight-bold">
-            Ristoranti, negozi di alimentari e supermercati
-          </h1>
-          <img src="../img/food.png" alt="immagine" />
-          <p class="py-3">
-            Dal 2013, la rete di ristoranti, negozi di alimentari e supermercati
-            con cui collaboriamo si è espansa fino a contare ora 160.000
-            partner. Il nostro servizio di consegna a domicilio è partito con i
-            ristoranti, dalle catene più famose ai piccoli ristoranti
-            indipendenti amati dai clienti a livello locale. Più del 90% dei
-            partner nella nostra piattaforma ha iniziato con il delivery proprio
-            grazie a noi. Il nostro modello ha fatto aumentare esponenzialmente
-            il numero di ristoranti che offrono le consegne a domicilio,
-            spalancando le porte a un mondo di opzioni e di gusto per i clienti
-            di tutto il mondo. Con l’obiettivo di diventare la food company per
-            eccellenza, nel 2018 abbiamo iniziato ad aggiungere negozi e
-            supermercati partner nella nostra piattaforma e a offrire un
-            servizio di spesa a domicilio tramite la stessa tecnologia logistica
-            al cuore del marketplace per i ristoranti. Oggi collaboriamo con
-            marchi tra i più famosi al mondo, tra cui Conad, Carrefour e Fresco
-            Market in Italia.
-          </p>
-          <!-- Rider -->
-          <h1 id="rider" class="py-3 font-weight-bold">Rider</h1>
-          <img src="../img/rider.png" alt="immagine" />
-          <p class="py-3">
-            La nostra proposta ai rider si basa su ciò che conta di più per
-            loro. Come facciamo a sapere di cosa si tratta? Chiediamo e
-            ascoltiamo sempre la loro opinione. In questo modo, abbiamo potuto
-            sviluppare un’offerta che mette in primo piano ciò di cui hanno
-            bisogno: orari flessibili, guadagni buoni e sicurezza. Deliveboo è
-            stata fra le prime piattaforme di consegne a domicilio al mondo a
-            offrire l’assicurazione contro gli infortuni personali (oggi in
-            Italia è fornita da INAIL) e un’assicurazione ulteriore per
-            eventuali danni a cose o a terzi. La copertura è offerta a titolo
-            gratuito e protegge i rider fin dalla loro prima consegna con
-            Deliveboo. Ciro, fondatore e CEO di Deliveboo, è stato il nostro
-            primo rider e ha effettuato consegne a tempo pieno durante il primo
-            anno di attività dell’azienda. Insieme ad altri membri del nostro
-            team, ad oggi consegna ancora a domicilio a cadenza regolare.
-          </p>
-          <!-- Tecnologia -->
-          <h1 id="technology" class="py-3 font-weight-bold">
-            La tecnologia ci guida
-          </h1>
-          <img src="../img/technology.png" alt="immagine" />
-          <p class="py-3">
-            La tecnologia sviluppata da Deliveboo permette di portare a termine
-            contemporaneamente centinaia di migliaia di ordini in tutto il
-            mondo, in modo intuitivo e affidabile per i rider, per i ristoranti
-            e per i supermercati. La nostra tecnologia ha permesso di scoprire
-            nuove opportunità di crescita per i ristoranti e i supermercati
-            attraverso il delivery, spesso per la prima volta; nuove opportunità
-            di lavoro flessibile per i rider; e per i clienti, la possibilità di
-            godersi un’esperienza di delivery fantastica. L’innovativa
-            tecnologia logistica di Deliveboo è basata su un machine learning
-            intelligente, che ci permette di migliorare costantemente ogni
-            piccolo aspetto del delivery, quartiere dopo quartiere. Deliveboo ha
-            sviluppato tecnologie come quella del nostro algoritmo Frank, che ci
-            permette di individuare il modo migliore di mettere in connessione
-            rider, ristoranti e clienti per dar vita a un servizio leader nel
-            settore. Frank migliora costantemente la sua precisione - come, ad
-            esempio, il tempo che necessita un ristorante per la preparazione di
-            un piatto o la stima del tempo di percorrenza di un determinato
-            itinerario in momenti diversi della giornata - per rendere il
-            servizio ancora più efficiente.
-          </p>
-          <!-- Innovazione -->
-          <h1 id="innovation" class="py-3 font-weight-bold">
-            Innovazione, a domicilio
-          </h1>
-          <img src="../img/delivery.png" alt="immagine" />
-          <p class="py-3">
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-12 col-xl-4" id="img-hover">
+          <figure>
+            <img class="rounded" src="../img/ceo.png" alt="" />
+            <div class="text-on-hover">
+              <h3 class="font-weight-bold">CEO di Deliveboo</h3>
+              <div>
+                <p>
+                  Il CEO di Deliveboo ha deciso che la sua missione personale
+                  sarebbe stata quella di portare i migliori ristoranti della
+                  città direttamente nelle case delle persone.
+                </p>
+              </div>
+            </div>
+          </figure>
+        </div>
+        <div class="col-12 col-xl-4" id="img-hover">
+          <figure>
+            <img class="rounded" src="../img/client.png" alt="" />
+            <div class="text-on-hover">
+              <h3 class="font-weight-bold">Clienti</h3>
+              <div>
+                <p>
+                  Da anni lavoriamo sodo per capire cosa vogliono i clienti e
+                  per offrire una proposta adatta a loro sia a livello
+                  geografico, sia individuale.
+                </p>
+              </div>
+            </div>
+          </figure>
+        </div>
+        <div class="col-12 col-xl-4" id="img-hover">
+          <figure>
+            <img class="rounded" src="../img/food.png" alt="" />
+            <div class="text-on-hover">
+              <h3 class="font-weight-bold">Cibo</h3>
+              <div>
+                <p>
+                  Dal 2013, la rete di ristoranti, negozi di alimentari e
+                  supermercati con cui collaboriamo si è espansa fino a contare
+                  ora 160.000 partner.
+                </p>
+              </div>
+            </div>
+          </figure>
+        </div>
+      </div>
+    </div>
+
+    <div class="container py-5">
+      <div class="row align-item-center">
+        <div class="col-12 col-lg-6">
+          <img class="rounded" src="../img/rider.png" alt="" />
+        </div>
+        <div class="col-12 col-lg-6 mt-3">
+          <div class="row">
+            <div class="col-12 col-sm-6 px-4 pb-4">
+              <h3 class="font-weight-bold mb-3">Origini</h3>
+              <p>
+                Quando il fondatore e CEO di Deliveboo Giorgio Surace si è
+                trasferito a Londra nel 2013, ha trovato una città piena di
+                ristoranti incredibili, ma è rimasto stupito che davvero pochi
+                di questi consegnassero a domicilio.
+              </p>
+            </div>
+            <div class="col-12 col-sm-6 px-4 pb-4">
+              <h3 class="font-weight-bold mb-3">Obbiettivo</h3>
+              <p>
+                L’obiettivo principale di questa connessione è facilitare
+                un’esperienza di acquisto indimenticabile, in circa 30 minuti.
+                Offrendo una consegna veloce e affidabile.
+              </p>
+            </div>
+            <div class="col-12 col-sm-6 px-4 pb-4">
+              <h3 class="font-weight-bold mb-3">rider</h3>
+              <p>
+                La nostra proposta ai rider si basa su ciò che conta di più per
+                loro. Come facciamo a sapere di cosa si tratta? Chiediamo e
+                ascoltiamo sempre la loro opinione. In questo modo, abbiamo
+                potuto sviluppare un’offerta che mette in primo piano ciò di cui
+                hanno bisogno
+              </p>
+            </div>
+            <div class="col-12 col-sm-6 px-4 pb-4">
+              <h3 class="font-weight-bold mb-3">Primo rider</h3>
+              <p>
+                Giorgio, fondatore e CEO di Deliveboo, è stato il nostro primo
+                rider e ha effettuato consegne a tempo pieno durante il primo
+                anno di attività dell’azienda. Insieme ad altri membri del
+                nostro team, ad oggi consegna ancora a domicilio a cadenza
+                regolare.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-12">
+          <h3 class="font-weight-bold mb-3">Innovazione a domicilio</h3>
+          <p class="mb-5">
             La passione di Deliveboo è il cibo, con l’obiettivo di fornire alle
             persone una scelta sempre più ampia. Ma la missione di Deliveboo non
             è solo quella di far sì che i ristoranti e supermercati della tua
@@ -186,16 +132,67 @@
             negozi dedicati esclusivamente al delivery, che si chiama Deliveboo
             HOP.
           </p>
-          <!-- I nostri valori -->
-          <h1 id="values" class="py-3 font-weight-bold">I nostri valori</h1>
-          <img src="../img/values.png" alt="immagine" />
-          <p class="py-3">
-            I nostri valori sono al centro di tutto l’operato di Deliveboo. Ci
-            guidano ogni giorno per lavorare in squadra e prendere decisioni,
-            dalle scelte che hanno un impatto sulle assunzioni e sullo sviluppo
-            del personale, a quelle che scrivono il futuro dei prodotti
-            Deliveboo.
+          <h3 class="font-weight-bold mb-3">I nostri valori</h3>
+          <p class="mb-5">
+            La passione di Deliveboo è il cibo, con l’obiettivo di fornire alle
+            persone una scelta sempre più ampia. Ma la missione di Deliveboo non
+            è solo quella di far sì che i ristoranti e supermercati della tua
+            zona siano a tua disposizione dove vuoi e quando vuoi; vogliamo
+            spingerci più in là, portando nuovi ristoranti vicino a te perché tu
+            possa avere ancora più scelta quando si tratta di mangiare bene.
+            Deliveboo Editions, il concept di cucine dedicate esclusivamente al
+            delivery, è un’innovazione che abbiamo introdotto noi e che aiuta i
+            ristoranti ad espandersi in nuove zone, abbattendo costi e rischi.
+            Aiutiamo i ristoranti ad avviare cucine dedicate al delivery in
+            nuove zone, senza i costi che presupporrebbe una struttura in una
+            zona centrale. Deliveboo fornisce inoltre ai ristoranti dati e
+            informazioni perché conoscano le tipologie di cucina più gettonate
+            nelle varie zone: in questo modo i ristoranti crescono più in fretta
+            e i clienti hanno più scelta. Deliveboo sta inoltre investendo
+            nell’espansione del proprio servizio di spesa “on demand” offrendo
+            la consegna dalle catene di supermercati più conosciute a livello
+            nazionale, ma anche fornendo la consegna rapida dalla nostra rete di
+            negozi dedicati esclusivamente al delivery, che si chiama Deliveboo
+            HOP.
           </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container py-5 credits">
+      <div class="row justify-content-between">
+        <div class="col-12 text-center">
+          <h2 class="font-weight-bold pb-4">Il team di Deliveboo</h2>
+        </div>
+        <div class="col-12 col-sm-2 text-center">
+          <a href="https://github.com/giorgios17"
+            ><img class="rounded-circle zoom" src="../img/giorgio.jpg" alt=""
+          /></a>
+          <p class="mt-3 font-weight-bold">Giorgio surace</p>
+        </div>
+        <div class="col-12 col-sm-2 mt-3 text-center">
+          <a href="https://github.com/DavideVenturelli12"
+            ><img class="rounded-circle zoom" src="../img/davide.jpg" alt=""
+          /></a>
+          <p class="mt-3 font-weight-bold">Davide Venturelli</p>
+        </div>
+        <div class="col-12 col-sm-2 mt-3 text-center">
+          <a href="https://github.com/emicif"
+            ><img class="rounded-circle zoom" src="../img/emanuela.jpg" alt=""
+          /></a>
+          <p class="mt-3 font-weight-bold">Emanuela Cifolelli</p>
+        </div>
+        <div class="col-12 col-sm-2 mt-3 text-center">
+          <a href="https://github.com/Ciro97cu"
+            ><img class="rounded-circle zoom" src="../img/ciro.jpg" alt=""
+          /></a>
+          <p class="mt-3 font-weight-bold">Ciro Cusati</p>
+        </div>
+        <div class="col-12 col-sm-2 mt-3 text-center">
+          <a href="https://github.com/Lucavit9"
+            ><img class="rounded-circle zoom" src="../img/luca.jpg" alt=""
+          /></a>
+          <p class="mt-3 font-weight-bold">Luca Vitullo</p>
         </div>
       </div>
     </div>
@@ -211,30 +208,61 @@ export default {
 <style lang="scss" scoped>
 @import "/resources/sass/_variables";
 
-.bg {
-  background-image: url("../img/chi-siamo-bg.png");
-}
-.title {
-  color: $yellow;
-  text-shadow: none;
+.bg-blue {
+  background-color: $blue;
+  color: hsl(53deg 37% 77%);
+  padding: 0 15%;
 }
 
-h1 {
-  text-shadow: 2px 2px 5px $yellow;
-}
-a {
-  text-decoration: none;
-}
-a:hover {
-  color: $yellow;
-}
 img {
   width: 100%;
-  border-radius: 5px;
+}
+
+.text-on-hover {
+  color: white;
+  background-color: rgba(0, 0, 0, 0.55);
+}
+#img-hover figure {
+  display: block;
+  position: relative;
+  bottom: 10px;
+  text-align: center;
+  cursor: pointer;
+}
+
+#img-hover figure .text-on-hover {
+  display: block;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+}
+
+#img-hover figure .text-on-hover div {
+  max-height: 0;
+  overflow: hidden;
+  transition: max-height 0.6s cubic-bezier(0, 1, 0, 1) -0.1s;
+}
+
+#img-hover figure:hover .text-on-hover div {
+  max-height: 1200px;
+  transition-timing-function: cubic-bezier(0.5, 0, 1, 0);
+  transition-delay: 0s;
+}
+
+.credits {
+  border-top: 2px solid $blue;
 }
 
 p {
-  font-size: 16px;
-  line-height: 30px;
+  font-size: 1rem;
+  line-height: 20px;
+}
+
+.zoom {
+  transition: transform 0.25s ease-in-out;
+  &:hover {
+    transform: scale(1.1);
+  }
 }
 </style>
